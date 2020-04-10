4f9b2cef152d32cc91e50e895af00dafcbcc709d
suFredericD <ilric6666@gmail.com>
2020-04-10 10:43:49 +0200

    nouveau fichier : scripts/paging/headerHtml.php 		Script de création et renvoi du header html des pages

---
+++ scripts/paging/headerHtml.php
@@ -0,0 +1,38 @@
+<?php
+/***************************************************************************************
+ *   	Frédéric DANIAU
+ *                Projet :   La Petie Ebénisterie en Herbe 'lPEeh'
+ *                  Page :   headerHtml.php
+ *                Chemin :   lPEeh/scripts/paging/headerHtml.php
+ *                  Type :   script de pagination Html
+ *              Fonction :   renvoi de l'en-tête Html
+ *   Date mise en oeuvre :   10/04/2020
+ *          Dernière MàJ :   10/04/2020
+ ****************************************************************************************/
+/*** INCLUSIONS ***/
+
+?>
+<!DOCTYPE html>
+
+<!-- -- -- -- En-tête HTML -- -- -- -->
+<html lang="fr">
+ <head>
+  <title>La Petite Ebénisterie en Herbe - Accueil</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="La Petite Ebénisterie en Herbe">
+  <meta name="author" content="Frédéric DANIAU">
+  <link href="../config/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+  <meta name="keywords" content="ébénisterie, menuiserie, design, bois, boiserie">
+  <meta name="robots" content="all">
+  <link href="../media/icons/Flogo10a.ico" rel="icon" type="image/ico">
+  <link href="../config/css/general.css" rel="stylesheet" type="text/css">
+  <link href="../config/fontawesome/css/all.css" rel="stylesheet">
+ </head>
