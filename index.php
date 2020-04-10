fb93073f12820e7d18ffb47478f91a7a33dc505c
suFredericD <ilric6666@gmail.com>
2020-04-10 10:43:02 +0200

    nouveau fichier : index.php 		Création de la page d'accueil du site

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/***************************************************************************************
+ *   	Frédéric DANIAU
+ *                Projet :   La Petie Ebénisterie en Herbe 'lPEeh'
+ *                  Page :   index.php
+ *                Chemin :   lPEeh/index.php
+ *                  Type :   page utilisateur
+ *              Fonction :   page d'acueil
+ *   Date mise en oeuvre :   10/04/2020
+ *          Dernière MàJ :   10/04/2020
+ ****************************************************************************************/
+/*** INCLUSIONS ***/
+ require_once('scripts/paging/headerHtml.php');
