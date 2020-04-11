700cee5dd35aa8635ca54636ada239e9909abc5e
suFredericD <ilric6666@gmail.com>
2020-04-11 09:42:55 +0200

    nouveau fichier : scripts/paging/footerHtml.php 		Construction et affichage du footer Html

---
+++ scripts/paging/footerHtml.php
@@ -0,0 +1,29 @@
+<?php
+/***************************************************************************************
+ *   	Frédéric DANIAU
+ *                Projet :   La Petite Ebénisterie en Herbe 'lPEeh'
+ *                  Page :   footerHtml.php
+ *                Chemin :   lPEeh/scripts/paging/footerHtml.php
+ *                  Type :   script de pagination Html
+ *              Contexte :   php 7.4
+ *              Fonction :   renvoi du footer Html
+ *   Date mise en oeuvre :   11/04/2020
+ *          Dernière MàJ :   11/04/2020
+ ****************************************************************************************/
+/*** *** *** *** *** INCLUSIONS *** *** *** *** ***/
+
+/*** *** *** *** *** FONCTIONS *** *** *** *** ***/
+function fct_DisplayFooterHtml($objPageSelf){
+    
+?>
+<!-- -- -- -- -- -- -- -- Fin du contenu -- -- -- -- -- -- -- -->
+    </body>
+
+<!-- -- -- -- -- -- -- -- Pied de page HTML -- -- -- -- -- -- -- -->
+    <footer>
+    
+    </footer>
+<!-- -- -- -- -- -- -- -- Fin de page -- -- -- -- -- -- -- -->
+</html>
+<?php
+}
