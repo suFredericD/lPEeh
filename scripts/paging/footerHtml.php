ef51728120ca4a9ef542984dbae587b9cefefe2f
suFredericD <ilric6666@gmail.com>
2020-04-13 14:13:15 +0200

    Début de rédaction et affichage du pied de page 	modifié :         config/css/general.css 	modifié :         scripts/paging/footerHtml.php

--- scripts/paging/footerHtml.php
+++ scripts/paging/footerHtml.php
@@ -8,7 +8,7 @@
  *              Contexte :   php 7.4
  *              Fonction :   renvoi du footer Html
  *   Date mise en oeuvre :   11/04/2020
- *          Dernière MàJ :   11/04/2020
+ *          Dernière MàJ :   13/04/2020
  ****************************************************************************************/
 /*** *** *** *** *** INCLUSIONS *** *** *** *** ***/
 
@@ -18,10 +18,11 @@ function fct_DisplayFooterHtml($objPageSelf){
 ?>
 <!-- -- -- -- -- -- -- -- Fin du contenu -- -- -- -- -- -- -- -->
     </body>
-
 <!-- -- -- -- -- -- -- -- Pied de page HTML -- -- -- -- -- -- -- -->
-    <footer>
-    
+    <footer class="site-container">
+        <div class="row">
+            <div id="copyright" class="col-xl-12">La petite ébébnisterie en herbe &copy; 2020</div>
+        </div>
     </footer>
 <!-- -- -- -- -- -- -- -- Fin de page -- -- -- -- -- -- -- -->
 </html>
