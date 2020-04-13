65e3dc551d6b5a8bf2520103528b89de4b2bf6cb
suFredericD <ilric6666@gmail.com>
2020-04-13 13:37:07 +0200

    Modifiaction de la webfont et titrage du header graphique du site

--- index.php
+++ index.php
@@ -1,13 +1,31 @@
 <?php
 /***************************************************************************************
  *   	Frédéric DANIAU
- *                Projet :   La Petie Ebénisterie en Herbe 'lPEeh'
+ *                Projet :   La Petite Ebénisterie en Herbe 'lPEeh'
  *                  Page :   index.php
  *                Chemin :   lPEeh/index.php
  *                  Type :   page utilisateur
+ *              Contexte :   php 7.4
  *              Fonction :   page d'acueil
  *   Date mise en oeuvre :   10/04/2020
- *          Dernière MàJ :   10/04/2020
+ *          Dernière MàJ :   11/04/2020
  ****************************************************************************************/
-/*** INCLUSIONS ***/
- require_once('scripts/paging/headerHtml.php');
+/*** *** *** *** *** INCLUSIONS *** *** *** *** ***/
+function LoadClass($strClass){                      // Inclusion automatique des classes
+    require 'classes/' . $strClass . '.php';
+  }
+spl_autoload_register('LoadClass');                 // Enregistrement de la fonction en autoload pour appel dès instanciation d'une classe non déclarée
+
+require_once('scripts/paging/headerHtml.php');      // Script de construction du header html
+require_once('scripts/paging/footerHtml.php');      // Script de construction du footer html
+require_once('scripts/paging/siteHeader.php');      // Script de construction du cartouche d'en-tête du site
+
+/*** *** *** *** *** INITIALISATIONS ET INSTANCIATIONS *** *** *** *** ***/
+$objPageSelf = new Page($_SERVER['SCRIPT_NAME']);
+
+/*** *** *** *** *** APPEL DE FONCTIONS *** *** *** *** ***/
+fct_DisplayHeaderHtml($objPageSelf);          // Header Html
+
+fct_DisplaySiteHeader($objPageSelf);          // Cartouche d'en-tête du site
+
+fct_DisplayFooterHtml($objPageSelf);          // Footer Html
--- scripts/paging/headerHtml.php
+++ scripts/paging/headerHtml.php
@@ -1,38 +1,49 @@
 <?php
 /***************************************************************************************
  *   	Frédéric DANIAU
- *                Projet :   La Petie Ebénisterie en Herbe 'lPEeh'
+ *                Projet :   La Petite Ebénisterie en Herbe 'lPEeh'
  *                  Page :   headerHtml.php
  *                Chemin :   lPEeh/scripts/paging/headerHtml.php
  *                  Type :   script de pagination Html
+ *              Contexte :   php 7.4
  *              Fonction :   renvoi de l'en-tête Html
  *   Date mise en oeuvre :   10/04/2020
- *          Dernière MàJ :   10/04/2020
+ *          Dernière MàJ :   11/04/2020
  ****************************************************************************************/
-/*** INCLUSIONS ***/
+/*** *** *** *** *** INCLUSIONS *** *** *** *** ***/
 
+/*** *** *** *** *** FONCTIONS *** *** *** *** ***/
+function fct_DisplayHeaderHtml($objPageSelf){
+    $strFavIcon = $objPageSelf->getPicturesPath() . "logo.png";
+    $strGeneralCss = $objPageSelf->getCssPath() . "general.css";
+    $strBootstrapCss = $objPageSelf->getConfigPath() . "bootstrap/css/bootstrap.min.css";
+    $strFontawesomeCss = $objPageSelf->getConfigPath() . "fontawesome/css/all.css";
 ?>
 <!DOCTYPE html>
 
-<!-- -- -- -- En-tête HTML -- -- -- -->
+<!-- -- -- -- -- -- -- -- En-tête HTML -- -- -- -- -- -- -- -->
 <html lang="fr">
- <head>
-  <title>La Petite Ebénisterie en Herbe - Accueil</title>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="La Petite Ebénisterie en Herbe">
-  <meta name="author" content="Frédéric DANIAU">
-  <link href="../config/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <head>
+        <title>La Petite Ebénisterie en Herbe</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="La Petite Ebénisterie en Herbe">
+        <meta name="author" content="Frédéric DANIAU">
 <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
-  <meta name="keywords" content="ébénisterie, menuiserie, design, bois, boiserie">
-  <meta name="robots" content="all">
-  <link href="../media/icons/Flogo10a.ico" rel="icon" type="image/ico">
-  <link href="../config/css/general.css" rel="stylesheet" type="text/css">
-  <link href="../config/fontawesome/css/all.css" rel="stylesheet">
- </head>
+        <meta name="keywords" content="ébénisterie, menuiserie, design, bois, boiserie">
+        <meta name="robots" content="all">
+        <link href="<?php echo $strFavIcon;?>" rel="icon" type="image/ico">
+        <link href="<?php echo $strBootstrapCss;?>" rel="stylesheet"><!-- Css BootStrap Core -->
+        <link href="<?php echo $strFontawesomeCss;?>" rel="stylesheet"><!-- Css FontAwesome Core -->
+        <link href="<?php echo $strGeneralCss;?>" rel="stylesheet" type="text/css"><!-- Css général du site -->
+    </head>
+<!-- -- -- -- -- -- -- -- Corps de la page -- -- -- -- -- -- -- -->
+    <body id="site-container">
+<?php
+}
